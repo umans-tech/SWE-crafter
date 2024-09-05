4399a812a9da345ceaf49e51d60e71b5dfc63446
wassel alazhar <wassel.alazhar@gmail.com>
2024-09-05 18:18:36 +0200

    feat: customize frontend

--- sweagent/frontend/src/components/Header.js
+++ sweagent/frontend/src/components/Header.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "../static/header.css";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Header = () => {
-  return (
-    <>
-      <header>
-        <Link
-          to="https://github.com/princeton-nlp/swe-agent"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={logo}
-            style={{ height: 50, marginRight: 20 }}
-            alt="swe agent logo"
-          />
-        </Link>
-        {/* <Link to="/">
+    return (
+        <>
+            <header>
+                <Link
+                    to="https://github.com/umans-tech/SWE-crafter"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={logo}
+                        style={{height: 50, marginRight: 20}}
+                        alt="swe agent logo"
+                    />
+                </Link>
+                {/* <Link to="/">
           <button>Home</button>
         </Link>
         <Link
@@ -28,9 +28,14 @@ const Header = () => {
         >
           <button>Discord</button>
         </Link> */}
-      </header>
-    </>
-  );
+                <p style={{alignItems: "center"}}>
+                    SWE-crafter uses your LLM to automatically handle change requests. <br/>
+                    Focusing on a Test-First approach, it validates PRs with pre-written tests,
+                    offering high-quality feedback to correct hallucinations.
+                </p>
+            </header>
+        </>
+    );
 };
 
 export default Header;
